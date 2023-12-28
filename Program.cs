8aac151a729cc65e35380e4d07a79a6ab860d55a
Mathz-Soares <matheussou487@gmail.com>
2023-12-28 16:28:55 -0300

    meu primeiro commit

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("de qual numero deseja ver a tabuada?");
+    int numero = Convert.ToInt16(Console.ReadLine());
+Console.WriteLine("----------------------------------------------------------");
+for (int i = 0; i <= 10; i++)
+{     
+    int tabuada = numero * i;
+    Console.WriteLine("{0} x {1} = {2}",numero, i, tabuada);
+}
